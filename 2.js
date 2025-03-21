653c11c6cf82a69c459f2cc7e4ecf8078b496e73
Sindon20 <ginessindonjhamaicamae@gmail.com>
2025-03-21 21:20:09 +0800

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,6 @@
+let studentName = prompt("Enter your name:");
+let subjectTitle = prompt("Enter the subject title:");
+let classSchedule = prompt("Enter the schedule (Time, Days):");
+let classroom = prompt("Enter the classroom number:");
+let classInstructor = prompt("Enter the calss Instructor's name");
+console.log = ('${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.');
