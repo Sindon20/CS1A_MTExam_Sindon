a3f5880bd659c8a9fb40e9338c076a70b2a5e0db
Sindon20 <ginessindonjhamaicamae@gmail.com>
2025-03-21 21:21:36 +0800

    Add files via upload

---
+++ 5.js
@@ -0,0 +1,9 @@
+let favoriteColors =  [];
+
+for(let i = 0; i< 3; i++){
+    let color = prompt('Enter your favorite color (${i + 1}/3):');
+    favoriteColor.push(color);
+    console.log('Updated color list:', favoriteColor);
+}
+
+console.log("Your favorite colors are:", favoriteColor);
