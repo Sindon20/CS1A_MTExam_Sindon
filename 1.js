bbb51042e7ca5c9b49da4dd1d96f6680092674cb
Sindon20 <ginessindonjhamaicamae@gmail.com>
2025-03-21 21:19:08 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,10 @@
+let fullname = prompt("Enter your full name (fiirst and last):");
+let age = prompt("Enter your age:");
+let favoriteNumber = prompt("Enter your favorite number:");
+let favoriteColor = prompt("Enter your favorite color:");
+
+console.log("User Information:");
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
