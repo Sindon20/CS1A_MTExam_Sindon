599546155cce80fefb18ab639ca9b67e5cca5f72
Sindon20 <ginessindonjhamaicamae@gmail.com>
2025-03-21 21:20:40 +0800

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,18 @@
+let age = prompt("Enter your age:");
+age = Number(age);
+let category;
+if (age<5){
+    category = "Toddler(under 5 years)";
+} else if (age >= 5 && age <= 12) {
+    category = "Child (5-12 years)";
+} else if (age >= 13 && age <= 19){
+    category = "Teenage (13-19 years)";
+} else if (age >=20 && age <= 35) {
+    category = "Young Adult (20-35 years)";
+} else if (age >= 36 && age <= 60){
+    category = "Middle Aged (36-60 years)";
+} else {
+    category = "Senior (over 60 years)";
+}
+
+console.log('You belong to the category: $ {category}');
